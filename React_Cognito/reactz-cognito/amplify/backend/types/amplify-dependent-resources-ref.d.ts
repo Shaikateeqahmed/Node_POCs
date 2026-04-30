a095a2d40b3e650141811ef19906838b41157d35
Shaik Ateeq Ahmed <sahmed@ariqt.com>
2026-04-30 15:20:59 +0530

    Created React app with cognito and amplify

---
+++ React_Cognito/reactz-cognito/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactzcognito7f99a682": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
